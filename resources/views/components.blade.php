bc9b4a039465d90e84d7532be1f662c4877bdb1e
Angelo Arcillas <angeloarcillas64@gmail.com>
2023-04-29 11:05:39 +0800

    Add newsletter component

--- resources/views/components.blade.php
+++ resources/views/components.blade.php
@@ -17,6 +17,10 @@
         * {
             font-family: 'Inter', sans-serif;
         }
+
+        .bug {
+            border: 1px solid red;
+        }
     </style>
 </head>
 
@@ -26,19 +30,6 @@
 
         <section class="min-h-screen p-4 border-4 border-dashed border-green-400">
 
-                <div class="text-center">
-                    <p class="text-4xl font-semibold text-rose-400">404</p>
-                    <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
-                    <p class="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking
-                        for.</p>
-                    <div class="mt-10 flex items-center justify-center gap-x-6">
-                        <a href="#"
-                            class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go
-                            back home</a>
-                        <a href="#" class="text-sm font-semibold text-gray-900 hover:underline">Contact support <span
-                                aria-hidden="true">&rarr;</span></a>
-                    </div>
-                </div>
 
         </section>
     </div>
