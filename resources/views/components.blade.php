c205236304757334ad5074c040ca059678af5a87
Angelo Arcillas <angeloarcillas64@gmail.com>
2023-04-29 10:44:03 +0800

    setup tailwidncss

---
+++ resources/views/components.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel - Vue - Tailwindcss Components</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <style>
+        * {
+            font-family: 'Inter', sans-serif;
+        }
+    </style>
+</head>
+
+<body class="antialiased">
+    <div id="App" class="max-w-7xl mx-auto p-6 lg:p-8">
+        <h1 class="mb-12 text-4xl font-bold">Laravel - Vue - Tailwindcss Componets</h1>
+
+        <section>
+            qwrcqwr
+        </section>
+    </div>
+</body>
+
+</html>
